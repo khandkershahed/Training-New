19cd3ba8549fbb16ad6f7f5d6e84ae14601d90bc
ashiquzzaman77 <dev2.ngenit@gmail.com>
2024-06-13 10:31:14 +0600

    Service

--- resources/views/admin/pages/about/index.blade.php
+++ resources/views/admin/pages/about/index.blade.php
@@ -22,7 +22,7 @@
                     </span> Create
                 </a>
             </div> --}}
-            
+
         </div>
         <div class="card-body pt-0">
             <table id="kt_datatable_example_5" class="table table-striped table-row-bordered gy-5 gs-7 border rounded">
