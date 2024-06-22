25b034b07a4cea087053a9f1f56411325d1c9d1b
shahed141123 <khandkershahed23@gmail.com>
2024-06-22 11:21:49 +0600

    13-06-24

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -135,6 +135,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js" async></script>
 
     <script src="{{ asset('admin/js/custom.js') }}"></script>
+    <script src="{{ asset('admin/js/multiselect.js') }}"></script>
     <script src="{{ asset('admin/js/validate.min.js') }}"></script>
     <script src="{{ asset('admin/js/tagsinput.js') }}"></script>
 
