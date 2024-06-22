222d8d454d41fff07ca89da44a57477d65db9c72
shahed141123 <khandkershahed23@gmail.com>
2024-06-22 11:33:01 +0600

    13-06-24

--- resources/views/frontend/pages/about.blade.php
+++ resources/views/frontend/pages/about.blade.php
@@ -81,7 +81,7 @@
                 </div>
                 <div class="col-lg-6">
                     <div>
-                        <img src="{{ asset('upload/about/' . ($about->row_two_image ? $about->row_two_image : '')) }}"
+                        <img class="img-fluid" src="{{ asset('upload/about/' . ($about->row_two_image ? $about->row_two_image : '')) }}"
                             alt="Row Two Image">
 
                     </div>
