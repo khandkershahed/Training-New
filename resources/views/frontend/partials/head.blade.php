df86f79534cb7e1e54b5081080ec5a511b1895c5
shahed141123 <khandkershahed23@gmail.com>
2024-07-07 11:51:38 +0600

    single-page-shahed

--- resources/views/frontend/partials/head.blade.php
+++ resources/views/frontend/partials/head.blade.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>
-      {{ $site->site_name }}
+      {{ optional($setting)->site_favicon ?? 'NGenIT Training' }}
     </title>
     <link
       rel="shortcut icon"
