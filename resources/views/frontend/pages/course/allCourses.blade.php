1629e6adab64c2fcdc3e2b7ae434dd331cb66a69
Sazeduzzaman <sazeduzzaman@yahoo.com>
2024-08-10 15:11:42 +0600

    first commit

--- resources/views/frontend/pages/course/allCourses.blade.php
+++ resources/views/frontend/pages/course/allCourses.blade.php
@@ -90,122 +90,124 @@
     <section>
         <div class="container py-5">
             <div class="row">
-                {{-- <div class="col-lg-3 sidebar-area-cources">
-              <ul class="nav nav-tabs flex-column border-0 px-0" id="myTab" role="tablist">
-                <li class="nav-item" role="presentation">
-                  <button class="nav-link w-100 border-0 cources-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">
-                    Administration, Office Management &amp; Secretarial
-                  </button>
-                </li>
-                <li class="nav-item" role="presentation">
-                  <button class="nav-link w-100 border-0 cources-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false" tabindex="-1">
-                    Management, Leadership &amp; Strategy
-                  </button>
-                </li>
-                <li class="nav-item" role="presentation">
-                  <button class="nav-link w-100 border-0 cources-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false" tabindex="-1">
-                    Finance, Accounting &amp; Budgeting
-                  </button>
-                </li>
-              </ul>
-            </div> --}}
                 <div class="col-lg-12">
-                    <div class="tab-content" id="myTabContent">
-                        <div class="tab-pane fade active show" id="home" role="tabpanel" aria-labelledby="home-tab">
-                            <div class="row">
-
-                                @forelse ($courses as $course)
-                                    <div class="col-lg-3 mb-4">
-                                        <div class="card border-0 bg-light shadow-sm rounded-2">
-                                            <div class="card-header p-0 bg-transparent">
-
-                                                <div>
-                                                    <img class="rounded-2 img-fluid course-images"
-                                                        src="{{ asset(!empty($course->thumbnail_image) ? url('storage/course/' . $course->thumbnail_image) : url('images/no-shop-imge.png')) }}"
-                                                        alt="">
-                                                </div>
-
-                                                <div class="p-2 text-center">
-
-                                                    <small class="pe-3">
-                                                        <span class="cource-badge rounded-2">
-                                                            <i class="fa-solid fa-graduation-cap"></i>
-                                                            Batch 2
-                                                        </span>
-                                                    </small>
-                                                    <small class="pe-3">
-                                                        <span class="cource-badge rounded-2">
-                                                            {{-- <i class="fa-solid fa-chair pe-2" aria-hidden="true"></i> --}}
-                                                            <i class="fa-solid fa-user"></i>
-                                                            {{ $course->available_seats }}
-                                                        </span>
-                                                    </small>
-
-                                                    @php
-                                                        // Convert registration end date to Unix timestamp
-                                                        $registrationEndTimestamp = strtotime(
-                                                            $course->registration_end_date,
-                                                        );
-
-                                                        // Current time
-                                                        $currentTime = time();
-
-                                                        // Calculate remaining time in seconds
-                                                        $remainingTime = $registrationEndTimestamp - $currentTime;
-
-                                                        // Convert remaining time to days
-                                                        $remainingDays = floor($remainingTime / (60 * 60 * 24));
-                                                    @endphp
-
-                                                    @if ($remainingDays > 0)
-                                                        <small class="pe-3">
-                                                            <span class="course-badge rounded-2">
-                                                                {{-- <i class="far fa-clock " aria-hidden="true"></i> --}}
-                                                                <i class="fa-solid fa-clock"></i>
-
-                                                                {{ $remainingDays }} Days
-
-                                                            </span>
-                                                        </small>
-                                                    @endif
-
-
-                                                </div>
-
-                                            </div>
-                                            <div class="card-body">
-                                                <div class="course-title">
-                                                    <h6 class="mb-0">
-                                                        {{ $course->name }}
-                                                    </h6>
-                                                </div>
-                                                <div class="pt-2">
-                                                    <a href="{{ url('course' . '/' . $course->id . '/' . $course->slug) }}"
-                                                        class="btn btn-light border w-100 text-center">See Details
-                                                        <i class="fa-solid fa-arrow-right ps-2" aria-hidden="true"></i></a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                @empty
-                                    <div class="text-center py-3">
-                                        <img class="img-fluid" width="210px"
-                                            src="https://i.ibb.co/Vg8gqx5/hand-drawn-no-data-illustration-23-2150696455.jpg"
-                                            alt="No Content">
-                                    </div>
-                                    <h5 class="text-center text-warning">No Course available right now.</h5>
-                                @endforelse
-
+                    <div class="text-center w-75 w-lg-100 mx-auto mb-5">
+                        <h4>Explore Our Courses</h4>
+                        <p>Unlock your potential with our comprehensive range of courses. Whether you're looking to enhance
+                            your skills, pursue a new career path, or simply learn something new, our courses are designed
+                            to meet your needs. From beginner to advanced levels, explore a variety of subjects taught by
+                            industry experts. Start your learning journey today and achieve your goals with our
+                            high-quality, accessible, and engaging courses.</p>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center mb-3">
+                        <div class="d-flex">
+                            <div class="me-3">
+                                <select class="form-select primary-btn-one" aria-label="Default select example">
+                                    <option selected>Select Category</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </div>
+                            <div class="me-3">
+                                <select class="form-select primary-btn-one" aria-label="Default select example">
+                                    <option selected>Select Service</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
                             </div>
                         </div>
-                        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                            Management, Leadership &amp; Strategy
-                        </div>
-                        <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
-                            Finance, Accounting &amp; Budgeting
+                        <div>
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Recipient's username"
+                                    aria-label="Recipient's username" aria-describedby="basic-addon2">
+                                <span class="input-group-text primary-btn-one w-25" id="basic-addon2">
+                                    <i class="fa-solid fa-search"></i>
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
+                @forelse ($courses as $course)
+                    <div class="col-lg-3 mb-4">
+                        <div class="card border-0 bg-light shadow-sm rounded-2">
+                            <div class="card-header p-0 bg-transparent">
+
+                                <div>
+                                    <img class="rounded-2 img-fluid course-images"
+                                        src="{{ asset(!empty($course->thumbnail_image) ? url('storage/course/' . $course->thumbnail_image) : url('images/no-shop-imge.png')) }}"
+                                        alt="">
+                                </div>
+
+                                <div class="p-2 text-center">
+
+                                    <small class="pe-3">
+                                        <span class="cource-badge rounded-2">
+                                            <i class="fa-solid fa-graduation-cap"></i>
+                                            Batch 2
+                                        </span>
+                                    </small>
+                                    <small class="pe-3">
+                                        <span class="cource-badge rounded-2">
+                                            {{-- <i class="fa-solid fa-chair pe-2" aria-hidden="true"></i> --}}
+                                            <i class="fa-solid fa-user"></i>
+                                            {{ $course->available_seats }}
+                                        </span>
+                                    </small>
+
+                                    @php
+                                        // Convert registration end date to Unix timestamp
+                                        $registrationEndTimestamp = strtotime($course->registration_end_date);
+
+                                        // Current time
+                                        $currentTime = time();
+
+                                        // Calculate remaining time in seconds
+                                        $remainingTime = $registrationEndTimestamp - $currentTime;
+
+                                        // Convert remaining time to days
+                                        $remainingDays = floor($remainingTime / (60 * 60 * 24));
+                                    @endphp
+
+                                    @if ($remainingDays > 0)
+                                        <small class="pe-3">
+                                            <span class="course-badge rounded-2">
+                                                {{-- <i class="far fa-clock " aria-hidden="true"></i> --}}
+                                                <i class="fa-solid fa-clock"></i>
+
+                                                {{ $remainingDays }} Days
+
+                                            </span>
+                                        </small>
+                                    @endif
+
+
+                                </div>
+
+                            </div>
+                            <div class="card-body">
+                                <div class="course-title">
+                                    <h6 class="mb-0">
+                                        {{ $course->name }}
+                                    </h6>
+                                </div>
+                                <div class="pt-2">
+                                    <a href="{{ url('course' . '/' . $course->id . '/' . $course->slug) }}"
+                                        class="btn btn-light border w-100 text-center">See Details
+                                        <i class="fa-solid fa-arrow-right ps-2" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @empty
+                    <div class="text-center py-3">
+                        <img class="img-fluid" width="210px"
+                            src="https://i.ibb.co/Vg8gqx5/hand-drawn-no-data-illustration-23-2150696455.jpg"
+                            alt="No Content">
+                    </div>
+                    <h5 class="text-center text-warning">No Course available right now.</h5>
+                @endforelse
             </div>
         </div>
     </section>
--- resources/views/frontend/pages/service/allService.blade.php
+++ resources/views/frontend/pages/service/allService.blade.php
@@ -9,29 +9,51 @@
             <p>One-Stop Learning &amp; Development Solutions</p>
         </div> --}}
     </section>
-
-    {{-- <section style="background-color: #eee">
-        <div class="container">
-            <div class="text-center px-lg-5 py-lg-5 px-2 py-5">
-                <h3 class="section-title">Popular Services We Offer</h3>
-                <div class="divider"></div>
-                <p class="text-muted py-0 px-0 py-lg-3 px-lg-5">
-                    Promise Training and Consultancy offers short-term workshops and
-                    training seminars for various work streams including management,
-                    leadership, finance, HR and Oil &amp; Gas. Participants of our
-                    public training program will also have a chance to share their
-                    experiences with professionals from other organisations. This
-                    enables them to gain new perspectives on the common challenges
-                    they face at the workplace.
-                </p>
-            </div>
-        </div>
-    </section> --}}
     <!-- All Servises -->
     <section>
-        <div class="container py-5">
+        <div class="container py-5 pt-0">
             <div class="row">
-
+                <div class="col-lg-12 p-3">
+                    <div class="text-center w-75 w-lg-100 mx-auto my-5">
+                        <h4>Explore Our Courses</h4>
+                        <p>Unlock your potential with our comprehensive range of courses. Whether you're looking to enhance
+                            your skills, pursue a new career path, or simply learn something new, our courses are designed
+                            to meet your needs. From beginner to advanced levels, explore a variety of subjects taught by
+                            industry experts. Start your learning journey today and achieve your goals with our
+                            high-quality, accessible, and engaging courses.</p>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center mb-3">
+                        <div class="d-flex">
+                            <div class="me-3">
+                                <select class="form-select primary-btn-one" aria-label="Default select example">
+                                    <option selected>Select Category</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </div>
+                            <div class="me-3">
+                                <select class="form-select primary-btn-one" aria-label="Default select example">
+                                    <option selected>Select Service</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Recipient's username"
+                                    aria-label="Recipient's username" aria-describedby="basic-addon2">
+                                <span class="input-group-text primary-btn-one w-25" id="basic-addon2">
+                                    <i class="fa-solid fa-search"></i>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
                 @forelse ($services as $service)
                     <div class="col-md-3 col-sm-6">
                         <div class="box-service">
