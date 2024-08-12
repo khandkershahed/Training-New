4c7874b1a5c586335a2e1285f006a8a7af0eab85
ashiquzzaman77 <dev2.ngenit@gmail.com>
2024-08-12 12:56:42 +0600

    Update 12-08-2024 By Ashiquzzaman

--- database/migrations/2023_05_28_173624_create_admins_table.php
+++ database/migrations/2023_05_28_173624_create_admins_table.php
@@ -17,9 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('address');
-            $table->string('phone');
-            $table->string('image');
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('image')->nullable();
+            $table->string('role')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
