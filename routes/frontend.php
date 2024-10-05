33d6e8ed243cc39b0a576a6eea3f76ebde7342ef
ashiquzzaman77 <ashiquzzaman.rajib.cse@!gmail.com>
2024-10-05 09:38:47 +0600

    Update By Ashiquzzaman 05-10-2024

--- routes/frontend.php
+++ routes/frontend.php
@@ -67,11 +67,14 @@ Route::get('/privacy-policy', [HomeController::class, 'privacyPolicy'])->name('p
 
 Route::get('/contact/location', [HomeController::class, 'location'])->name('location');
 Route::get('support', [HomeController::class, 'Support'])->name('support');
-Route::get('/newsletter', [NewsletterController::class, 'newsletter']);
-Route::post('/newsletter/store', [NewsLetterController::class, 'store'])->name('newsletter.store');
 
+// Route::get('/newsletter', [NewsletterController::class, 'newsletter']);
+// Route::post('/newsletter/store', [NewsLetterController::class, 'store'])->name('newsletter.store');
+
+//About
 Route::get('/about-us', [HomeController::class, 'about'])->name('about');
 
+//Event
 Route::get('/events', [HomeController::class, 'event'])->name('event');
 Route::get('/events/{id}', [HomeController::class, 'eventDetails'])->name('event.details');
 Route::get('/event/registration', [HomeController::class, 'eventRegistration'])->name('event.registration');
