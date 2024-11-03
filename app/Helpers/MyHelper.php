3f6e8302bf6ae1faa7091c60c1e0c965b91ab612
shahed141123 <khandkershahed23@gmail.com>
2024-11-03 12:38:50 +0600

    first commit

--- app/Helpers/MyHelper.php
+++ app/Helpers/MyHelper.php
@@ -94,4 +94,54 @@ if (!function_exists('customUpload')) {
 
         return array_map('htmlspecialchars', $output);
     }
+
+}
+
+if (!function_exists('newUpload')) {
+    function newUpload(UploadedFile $mainFile, string $uploadPath, ?int $reqWidth = null, ?int $reqHeight = null): array
+    {
+        try {
+            $originalName     = pathinfo($mainFile->getClientOriginalName(), PATHINFO_FILENAME);
+            $fileExtention    = $mainFile->getClientOriginalExtension();
+            $currentTime      = Str::random(10) . time();
+            $name = Str::limit($originalName, 100);
+            $fileName = $currentTime . '.' . $fileExtention ;
+            $fullUploadPath  = "public/$uploadPath";
+
+            // Ensure directory exists
+            if (!Storage::exists($fullUploadPath)) {
+                // Create directory
+                $localPath = storage_path("app/$fullUploadPath");
+                if (!mkdir($localPath, 0755, true)) {
+                    abort(404, "Failed to create the directory: $fullUploadPath");
+                }
+                // Ensure directory permissions are set correctly
+                chmod($localPath, 0755);
+            }
+
+            // Store the file
+            try {
+                $mainFile->storeAs($fullUploadPath, $fileName);
+                $filePath = "$uploadPath/$fileName";
+            } catch (\Exception $e) {
+                abort(500, "Failed to store the file: " . $e->getMessage());
+            }
+
+            $output = [
+                'status'         => 1,
+                'file_name'      => $fileName,
+                'file_extension' => $mainFile->getClientOriginalExtension(),
+                'file_size'      => $mainFile->getSize(),
+                'file_type'      => $mainFile->getMimeType(),
+                'file_path'      => $filePath,
+            ];
+
+            return array_map('htmlspecialchars', $output);
+        } catch (\Exception $e) {
+            return [
+                'status' => 0,
+                'error_message' => $e->getMessage(),
+            ];
+        }
+    }
 }
