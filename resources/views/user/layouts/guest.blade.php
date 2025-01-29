b7ec188233822a50cdfad85d50bd7d5271d5d45d
shahed141123 <khandkershahed23@gmail.com>
2025-01-29 10:24:07 +0600

    26-01-25

--- resources/views/user/layouts/guest.blade.php
+++ resources/views/user/layouts/guest.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'NgenIt Training') }}</title>
+    <title>
+        {{ optional($setting)->site_name ?? 'NGenIT Training' }}
+    </title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
